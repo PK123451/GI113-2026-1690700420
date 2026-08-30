e8f5227c089fefe9ab2e3dbaacbde9e9e73a0412
PK123451 <pattarachanon.kras@bumail.net>
2026-08-30 22:09:28 +0700

    Update Program.cs

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -14,7 +14,35 @@ namespace Lab02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string bossName = "Kirin";
+            char rank = 'S';
+            int level = 7;
+            int maxHp = 240;
+            int currentHp = 175;
+            float attackPower = 42.5f;
+            double critMultiplier = 1.75;
+            bool isBoss = true;
+
+            Console.WriteLine("-- BOSS STATUS: INITIAL --");
+            Console.WriteLine($"Boss Name: {bossName}");
+            Console.WriteLine($"Rank: {rank}");
+            Console.WriteLine($"Level: {level}");
+            Console.WriteLine($"HP: {currentHp} / {maxHp}");
+           
+            int hpPercentage = currentHp * 100 / maxHp;
+            Console.WriteLine($"Attack Power: {attackPower}");
+            Console.WriteLine($"Critical Multiplier: {critMultiplier}");
+            Console.WriteLine($"IS BOSS: {isBoss}");
+            Console.WriteLine();
+            Console.WriteLine($"HP Percent: {hpPercentage}%");
+            Console.WriteLine();
+            Console.WriteLine("Kirin takes 60 damge!");
+            Console.WriteLine();
+            Console.WriteLine($"-- BOSS STATUS: AFTER DAMAGE --");
+            Console.WriteLine($"HP: { currentHp} / {maxHp}");
+            Console.WriteLine($"HP Percent: 47%");
+
+
         }
     }
 }
