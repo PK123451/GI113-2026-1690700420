f63f8e4ef13bf75f75431f860ff889492065834d
PK123451 <pattarachanon.kras@bumail.net>
2026-08-30 21:20:38 +0700

    Lab02

---
+++ Lab02/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Student ID : 1690700420
+ * Name       : ภัทรชนนท์ กระแสสินธุ์
+ * Section    : 129A
+ * No.        : 
+ * Course     : GI113 Computer Programming (GI)
+ */
+
+
+
+namespace Lab02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
