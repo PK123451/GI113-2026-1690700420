7f3c584e0e04438395a6b938d1c5f355e34273a2
PK123451 <pattarachanon.kras@bumail.net>
2026-09-03 20:55:45 +0700

    Update Program.cs

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -43,6 +43,93 @@ namespace Lab02
             Console.WriteLine($"HP Percent: 47%");
 
 
+
+            string Name = "PK";
+            char Rank = 'S';
+            int Level = 10;
+            int MaxHp = 250;
+            float AttackPower = 45;
+            double CritMultiplier = 1.75;
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Rank: {Rank}");
+            Console.WriteLine($"Level: {Level}");
+            Console.WriteLine($"Max HP: {MaxHp}");
+            Console.WriteLine($"Attack Power: {AttackPower}");
+            Console.WriteLine($"Critical Multiplier: {CritMultiplier}");
+
+
+
+            string name = "Aom";
+            char RAnk = 'A';
+            int LEvel = 8;
+            int MAxHp = 200;
+            float ATtackPower = 25;
+            double CRitMultiplier = 1;
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"rank: {RAnk}");
+            Console.WriteLine($"level: {LEvel}");
+            Console.WriteLine($"max HP: {MAxHp}");
+            Console.WriteLine($"attack power: {ATtackPower}");
+            Console.WriteLine($"critical multiplier: {CRitMultiplier}");
+
+
+            string name1 = "Kai";
+            char rank1 = 'B';
+            int level1 = 7;
+            int maxHp1 = 190;
+            float attackPower1 = 23;
+            double critMultiplier1 = 0.9;
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine($"Name: {name1}");
+            Console.WriteLine($"Rank: {rank1}");
+            Console.WriteLine($"Level: {level1}");
+            Console.WriteLine($"Max HP: {maxHp1}");
+            Console.WriteLine($"Attack Power: {attackPower1}");
+            Console.WriteLine($"Critical Multiplier: {critMultiplier1}");
+
+
+            string name2 = "Joke";
+            char rank2 = 'D';
+            int level2 = 5;
+            int maxHp2 = 150;
+            float attackPower2 = 16;
+            double critMultiplier2 = 0.9;
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine($"Name: {name2}");
+            Console.WriteLine($"Rank: {rank2}");
+            Console.WriteLine($"Level: {level2}");
+            Console.WriteLine($"Max HP: {maxHp2}");
+            Console.WriteLine($"Attack Power: {attackPower2}");
+            Console.WriteLine($"Critical Multiplier: {critMultiplier2}");
+
+
+            string name3 = "Aon";
+            char rank3 = 'F';
+            int level3 = 6;
+            int maxHp3 = 160;
+            float attackPower3 = 15;
+            double critMultiplier3 = 0.9;
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine($"Name: {name3}");
+            Console.WriteLine($"Rank: {rank3}");
+            Console.WriteLine($"Level: {level3}");
+            Console.WriteLine($"Max HP: {maxHp3}");
+            Console.WriteLine($"Attack Power: {attackPower3}");
+            Console.WriteLine($"Critical Multiplier: {critMultiplier3}");
+
+
         }
     }
 }
