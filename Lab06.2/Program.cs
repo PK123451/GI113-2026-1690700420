d5fd855b35e2556eea591750e577de5f3a5ab027
PK123451 <pattarachanon.kras@bumail.net>
2026-09-25 16:16:46 +0700

    Lab6

---
+++ Lab06.2/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+* Student ID : 1690700420
+* Name       : ภัทรชนนท์ กระแสสินธุ์
+* Section    : 129A
+* No.        : 23
+* Course     : GI113 Computer Programming (GI)
+*/
+
+
+namespace Lab06._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "PK";
+            char rank = 'S';
+            int leve = 5;
+            int hp = 120;
+            float attackPower = 20;
+
+            Console.WriteLine("===== PLAYER STATUS =====");          
+            Console.WriteLine($"Name: {name} ");
+            Console.WriteLine($"Rank: {rank} ");
+            Console.WriteLine($"Leve: {leve} ");
+            Console.WriteLine($"HP : {hp} ");
+            Console.WriteLine($"Attack : {attackPower}");
+            Console.WriteLine("=========================");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            string Name = "Dark Slime";
+            char Rank = 'B';
+            int Leve = 10;
+            int HP = 150;
+            float AttackPower = 10;
+            Console.WriteLine("====== ENEMY STATUS ======");
+            Console.WriteLine($"Name: {Name} ");
+            Console.WriteLine($"Rank: {Rank} ");
+            Console.WriteLine($"Leve: {Leve} ");
+            Console.WriteLine($"HP : {HP} ");
+            Console.WriteLine($"Attack : {AttackPower}");
+            Console.WriteLine("==========================");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("===== PLAYER TURN =====");
+
+
+
+
+
+
+        }
+    }
+}
